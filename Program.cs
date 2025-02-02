837c1b6968642bcbbf09962034d13c75320ee0a3
uditha bandara <wijaya.uditha98@gmail.com>
2025-02-02 12:32:15 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using CSharpPractice.Threads;
+
+namespace CSharpPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //thread practice
+            ThreadingPractice tp = new ThreadingPractice();
+            tp.ExecuteThread();
+        }
+    }
+}
---
+++ Threads/ThreadingPractice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.Threads
+{
+    public class ThreadingPractice
+    {
+        public void DoWork()
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"Separate thread number {i}");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void ExecuteThread()
+        {
+            Thread thread = new Thread(DoWork);
+            thread.Start();
+
+            Console.WriteLine("Main thread is running.....");
+
+            for (int i = 0; i <= 5; i++)
+            {
+                Console.WriteLine($"Main thread number {i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
