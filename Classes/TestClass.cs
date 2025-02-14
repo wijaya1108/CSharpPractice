f957b28c11fdb2a13087d1eb3073d9228e70fcee
uditha bandara <wijaya.uditha98@gmail.com>
2025-02-14 23:12:50 +0530

    differences between class and struct

---
+++ Classes/TestClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractice.Classes
+{
+    public class TestClass
+    {
+        public int NumericValue { get; set; }
+        public string StringValue { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            var other = (TestClass)obj;
+
+            return NumericValue == other.NumericValue && StringValue == other.StringValue;
+        }
+    }
+}
